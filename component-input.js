2010fdde094a8e5d27e83262490a4a036c62d477
David <david.89.mora@gmail.com>
2019-09-04 18:55:47 -0500

    segundo commit

--- component-input.js
+++ component-input.js
@@ -7,9 +7,7 @@ export default {
         <div class="form-group" style="margin-bottom:0">
             <input class="form-control" placeholder="Agregar Item" v-model="edit1">
         </div>
-        <button class=" btn-block btn btn-success col-md-4 mt-3" @click="$emit('addtarea')">
-                    Agregar
-                </button>
+        
 
         </form>
     
